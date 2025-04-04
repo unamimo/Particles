2f4f8ed88dfdfeedc0c6684026e258e95ccadb2a
Millie <57187866+unamimo@users.noreply.github.com>
2025-04-04 16:10:46 +0100

    chore: setup project

--- Particles/main.cpp
+++ Particles/main.cpp
@@ -1,24 +1,29 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
+#include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-            /*
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-            */
+    //while (window.isOpen())
+    //{
+    //        /*
+    //    while (const std::optional event = window.pollEvent())
+    //    {
+    //        if (event->is<sf::Event::Closed>())
+    //            window.close();
+    //    }
+    //        */
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    //    //window.clear();
+    //    //window.draw(shape);
+    //    //window.display();
+    //}
+
+    Game game;
+    game.run();
 }
